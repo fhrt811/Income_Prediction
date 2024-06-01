eac231b115db01742d94dc8ba78930b3c2a20e06
fhrt811 <fhrt811@gmail.com>
2024-06-01 12:16:47 +0530

    Data Ingestion

--- src/component/data_ingestion.py
+++ src/component/data_ingestion.py
@@ -52,5 +52,5 @@ if __name__ == "__main__":
     obj=DataIngestion()
     train_data,test_data = obj.initiate_data_ingestion()
     # # Running data_transformation
-    #Data_transformation=DataTransformation()
-    #train_arr,test_arr,_=Data_transformation.initiate_data_transformation(train_data,test_data)
+    Data_transformation=DataTransformation()
+    train_arr,test_arr,_=Data_transformation.initiate_data_transformation(train_data,test_data)
